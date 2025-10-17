3b6d05233d2f2d62b8324246d281ef09a8373b02
ysa9-ua <124249135+ysa9-ua@users.noreply.github.com>
2025-10-17 18:02:08 +0200

    Update EV_Central.java

--- p2/central/EV_Central.java
+++ p2/central/EV_Central.java
@@ -1,5 +1,31 @@
-package p2.central;
+package p2.evcharging.central;
+
+import common.util.KafkaProducerHelper;
+import common.util.KafkaConsumerHelper;
+
+import java.util.Arrays;
 
 public class EV_Central {
 
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Uso: java p2.evcharging.central.EV_Central <bootstrapServers>");
+            System.out.println("Ejemplo: java p2.evcharging.central.EV_Central localhost:9092");
+            System.exit(1);
+        }
+
+        String bootstrapServers = args[0];
+
+        System.out.println("EV_Central conectado a Kafka en " + bootstrapServers);
+
+        KafkaProducerHelper producer = new KafkaProducerHelper(bootstrapServers);
+        KafkaConsumerHelper consumer = new KafkaConsumerHelper(
+                bootstrapServers,
+                "central-group",
+                Arrays.asList("cp-to-central")
+        );
+
+        ServidorConcurrente servidor = new ServidorConcurrente(producer, consumer);
+        servidor.iniciar();
+    }
 }
