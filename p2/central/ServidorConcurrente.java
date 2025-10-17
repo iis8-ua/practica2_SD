9d380ed006f154e22509e27e1c08c42f99b98a1a
ysa9-ua <124249135+ysa9-ua@users.noreply.github.com>
2025-10-17 18:03:01 +0200

    Update ServidorConcurrente.java

--- p2/central/ServidorConcurrente.java
+++ p2/central/ServidorConcurrente.java
@@ -1,5 +1,24 @@
-package p2.central;
+package p2.evcharging.central;
+
+import common.util.KafkaProducerHelper;
+import common.util.KafkaConsumerHelper;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 public class ServidorConcurrente {
 
+    private final KafkaProducerHelper producer;
+    private final KafkaConsumerHelper consumer;
+
+    public ServidorConcurrente(KafkaProducerHelper producer, KafkaConsumerHelper consumer) {
+        this.producer = producer;
+        this.consumer = consumer;
+    }
+
+    public void iniciar() {
+        System.out.println("🛰️ ServidorConcurrente escuchando mensajes Kafka...");
+        consumer.pollLoop(record -> {
+            Thread hilo = new Thread(new HiloServidor(record, producer));
+            hilo.start();
+        });
+    }
 }
