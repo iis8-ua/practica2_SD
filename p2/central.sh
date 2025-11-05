f1996590259d5ac1579f7b9f7000986ea14dcf36
Israel Izquierdo Sanchís <iis8@alu.ua.es>
2025-11-05 14:41:10 +0100

    Se han creado los .sh para ejecutar de manera rápida la central, los monitores y los engine, falta meter el driver

--- p2/central.sh
+++ p2/central.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-javac -cp "libs/*" p2/db/DBManager.java p2/central/*.java && \
-java -cp ".:libs/*" p2.central.EV_Central localhost:9092
+java -cp "../p2/libs/*:.." p2.central.EV_Central localhost:9092
